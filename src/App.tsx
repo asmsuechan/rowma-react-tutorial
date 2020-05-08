decacc40cc8a118e9ef865d912ce549349c550e3
asmsuechan <suenagaryoutaabc@gmail.com>
2020-05-08 11:25:32 +0900

    Connect to a robot

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
   const [robotUuids, setRobotUuids] = useState<Array<string>>([]);
   const [selectedRobotUuid, setSelectedRobotUuid] = useState<string>('');
 
+  const [robot, setRobot] = useState<any>(null);
+
   useEffect(() => {
     const _rowma = new Rowma();
     setRowma(_rowma);
@@ -19,6 +21,22 @@ function App() {
 
   const handleConnectionListChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedRobotUuid(event.target.value)
+  }
+
+  const handleConnectClicked = () => {
+    rowma.connect(selectedRobotUuid).then((sock: any) => {
+      setSocket(sock)
+    }).catch((e: any) => {
+      console.log(e)
+    })
+
+    rowma.getRobotStatus(selectedRobotUuid).then((res: any) => {
+      setRobot(res.data)
+    })
+  }
+
+  const handleRostopicChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(event)
   }
 
   return (
@@ -33,6 +51,24 @@ function App() {
           })
         )}
       </select>
+      <button
+        disabled={selectedRobotUuid === ''}
+        onClick={handleConnectClicked}
+      >
+        Connect
+      </button>
+      <div>
+        {robot && robot.rostopics.length > 0 && (
+      	  <select onChange={handleRostopicChange}>
+      	    <option value=''>{''}</option>
+            {robot.rostopics.map((topic: string) => {
+      	      return(
+      	        <option key={topic} value={topic}>{topic}</option>
+      	      )
+      	    })}
+      	  </select>
+        )}
+      </div>
     </div>
   );
 }
